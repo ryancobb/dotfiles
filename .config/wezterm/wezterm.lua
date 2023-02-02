f43dcbb5c4e35b1213295f8fe928879d851d9e41
Ryan Cobb <rcobb@gitlab.com>
2023-02-02 13:28:24 -0800

    sync

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,38 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+local function isViProcess(pane)
+  -- get_foreground_process_name On Linux, macOS and Windows,
+  -- the process can be queried to determine this path. Other operating systems
+  -- (notably, FreeBSD and other unix systems) are not currently supported
+  return pane:get_foreground_process_name():find('n?vim') ~= nil
+  -- return pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+  if isViProcess(pane) then
+    window:perform_action(
+      act.SendKey({ key = vim_direction, mods = 'CTRL' }),
+      pane
+    )
+  else
+    window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+  end
+end
+
+wezterm.on('ActivatePaneDirection-right', function(window, pane)
+  conditionalActivatePane(window, pane, 'Right', 'l')
+end)
+wezterm.on('ActivatePaneDirection-left', function(window, pane)
+  conditionalActivatePane(window, pane, 'Left', 'h')
+end)
+wezterm.on('ActivatePaneDirection-up', function(window, pane)
+  conditionalActivatePane(window, pane, 'Up', 'k')
+end)
+wezterm.on('ActivatePaneDirection-down', function(window, pane)
+  conditionalActivatePane(window, pane, 'Down', 'j')
+end)
+
 return {
   color_scheme = 'nordfox',
   font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Medium' }),
@@ -47,12 +79,12 @@ return {
     { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = true } },
     { key = 's', mods = 'CMD|SHIFT', action = act.SplitPane { direction = 'Down', size = { Percent = 20 } }, },
     { key = 'v', mods = 'CMD|SHIFT', action = act.SplitPane { direction = 'Right' }, },
-    { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left', },
-    { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right', },
-    { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up', },
-    { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down', },
     { key = 'b', mods = 'CTRL|SHIFT', action = act.RotatePanes 'CounterClockwise' },
     { key = 'n', mods = 'CTRL|SHIFT', action = act.RotatePanes 'Clockwise' },
+    { key = 'h', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-left') },
+    { key = 'l', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-right') },
+    { key = 'k', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-up') },
+    { key = 'j', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-down') },
     { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
     { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
     { key = 'UpArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
