d83cf7cd1f9ea82381c07da12c6c876b0f19edd5
Ryan Cobb <rcobb@gitlab.com>
2023-03-31 11:04:51 -0700

    sync

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,37 +1,45 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local function isViProcess(pane)
-  -- get_foreground_process_name On Linux, macOS and Windows,
-  -- the process can be queried to determine this path. Other operating systems
-  -- (notably, FreeBSD and other unix systems) are not currently supported
-  return pane:get_foreground_process_name():find('n?vim') ~= nil
-  -- return pane:get_title():find("n?vim") ~= nil
+local function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
 end
 
-local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
-  if isViProcess(pane) then
-    window:perform_action(
-      act.SendKey({ key = vim_direction, mods = 'CTRL' }),
-      pane
-    )
-  else
-    window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
-  end
+local function is_vim(pane)
+  local process_name = basename(pane:get_foreground_process_name())
+  return process_name == 'nvim' or process_name == 'vim'
 end
 
-wezterm.on('ActivatePaneDirection-right', function(window, pane)
-  conditionalActivatePane(window, pane, 'Right', 'l')
-end)
-wezterm.on('ActivatePaneDirection-left', function(window, pane)
-  conditionalActivatePane(window, pane, 'Left', 'h')
-end)
-wezterm.on('ActivatePaneDirection-up', function(window, pane)
-  conditionalActivatePane(window, pane, 'Up', 'k')
-end)
-wezterm.on('ActivatePaneDirection-down', function(window, pane)
-  conditionalActivatePane(window, pane, 'Down', 'j')
-end)
+local direction_map = {
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+  LeftArrow = 'Left',
+  RightArrow = 'Right',
+  DownArrow = 'Down',
+  UpArrow = 'Up'
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = 'CTRL',
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        win:perform_action({
+          SendKey = { key = key, mods = 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_map[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_map[key] }, pane)
+        end
+      end
+    end),
+  }
+end
 
 return {
   color_scheme = 'nordfox',
@@ -77,20 +85,25 @@ return {
   },
   keys = {
     { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = true } },
-    { key = 'h', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-left') },
-    { key = 'l', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-right') },
-    { key = 'k', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-up') },
-    { key = 'j', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-down') },
-    { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
-    { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
-    { key = 'UpArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
-    { key = 'DownArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
+    -- { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
+    -- { key = 'RightArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
+    -- { key = 'UpArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
+    -- { key = 'DownArrow', mods = 'CTRL|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
     { key = '0', mods = 'CTRL|SHIFT', action = act.PaneSelect { mode = 'SwapWithActive' } },
     { key = 'K', mods = 'CTRL|SHIFT', action = act.ClearScrollback 'ScrollbackAndViewport' },
     { key = 'Z', mods = 'CTRL|SHIFT', action = act.TogglePaneZoomState },
     { key = 'B', mods = 'CTRL|SHIFT', action = act.RotatePanes 'CounterClockwise' },
     { key = 'N', mods = 'CTRL|SHIFT', action = act.RotatePanes 'Clockwise' },
     { key = 'S', mods = 'CMD|SHIFT', action = act.SplitPane { direction = 'Down', size = { Percent = 20 } }, },
-    { key = 'V', mods = 'CMD|SHIFT', action = act.SplitPane { direction = 'Right' }, }
+    { key = 'V', mods = 'CMD|SHIFT', action = act.SplitPane { direction = 'Right' }, },
+    split_nav('move', 'h'),
+    split_nav('move', 'j'),
+    split_nav('move', 'k'),
+    split_nav('move', 'l'),
+    split_nav('resize', 'LeftArrow'),
+    split_nav('resize', 'RightArrow'),
+    split_nav('resize', 'UpArrow'),
+    split_nav('resize', 'DownArrow'),
+
   }
 }
