cbabc85221e535a33e4f62509a4a5676169c0f69
Ryan Cobb <rcobb@gitlab.com>
2025-01-30 20:37:21 -0700

    sync

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -17,23 +17,16 @@ return {
           all = {
             NeoTreeNormal = { link = "Normal" },
             NeoTreeNormalNC = { link = "NormalNC" },
-            WindowPickerStatusLine = { link = "@text.warning" },
-            WindowPickerStatusLineNC = { link = "@text.warning" },
-            ["@comment.todo"] = { link = "@comment" },
-            ["@lsp.type.namespace.ruby"] = { link = "@type.ruby" },
-            ["@lsp.type.parameter.ruby"] = { link = "@variable.ruby" },
-            DiffAdd = { bg = C(spec.bg1):blend(C(palette.green.dim), 0.25):to_css() },
-            DiffChange = { bg = C(spec.bg1):blend(C(palette.yellow.dim), 0.15):to_css() },
-            DiffDelete = { bg = C(spec.bg1):blend(C(palette.red.dim), 0.25):to_css() },
-            DiffText = { bg = C(spec.bg1):blend(C(palette.yellow.dim), 0.3):to_css() },
-            Folded = { bg = "none" },
-            RainbowDelimiterRed = { link = "TSRainbowRed" },
-            RainbowDelimiterBlue = { link = "TSRainbowBlue" },
-            RainbowDelimiterCyan = { link = "TSRainbowCyan" },
-            RainbowDelimiterGreen = { link = "TSRainbowGreen" },
-            RainbowDelimiterOrange = { link = "TSRainbowOrange" },
-            RainbowDelimiterViolet = { link = "TSRainbowViolet" },
-            RainbowDelimiterYellow = { link = "TSRainbowYellow" },
+            --   WindowPickerStatusLine = { link = "@text.warning" },
+            --   WindowPickerStatusLineNC = { link = "@text.warning" },
+            --   ["@comment.todo"] = { link = "@comment" },
+            --   ["@lsp.type.namespace.ruby"] = { link = "@type.ruby" },
+            --   ["@lsp.type.parameter.ruby"] = { link = "@variable.ruby" },
+            --   DiffAdd = { bg = C(spec.bg1):blend(C(palette.green.dim), 0.25):to_css() },
+            --   DiffChange = { bg = C(spec.bg1):blend(C(palette.yellow.dim), 0.15):to_css() },
+            --   DiffDelete = { bg = C(spec.bg1):blend(C(palette.red.dim), 0.25):to_css() },
+            --   DiffText = { bg = C(spec.bg1):blend(C(palette.yellow.dim), 0.3):to_css() },
+            --   Folded = { bg = "none" },
           },
         },
       }
@@ -57,7 +50,7 @@ return {
           hl["@string.special.symbol.ruby"] = { link = "Special" }
           hl.SnacksPicker = { link = "Normal" }
           hl.SnacksPickerBorder = { link = "Normal" }
-          hl.SnacksPickerTitle = { link = "TSNote" }
+          hl.SnacksPickerTitle = { link = "Todo" }
         end,
       })
     end,
