249788feb4f85cdedfe852b81d94daa0d40d2bd5
Ryan Cobb <rcobb@gitlab.com>
2025-02-08 17:21:10 -0700

    sync

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -12,26 +12,22 @@ return {
       desc = "alternate",
     },
   },
-  opts = function(_, opts)
-    if LazyVim.has("trouble.nvim") then
-      return vim.tbl_deep_extend("force", opts or {}, {
-        dashboard = { enabled = false },
-        picker = {
-          actions = require("trouble.sources.snacks").actions,
-          matcher = { frecency = true },
-          formatters = { file = { truncate = 80 } },
-          win = {
-            input = {
-              keys = {
-                ["<c-t>"] = {
-                  "trouble_open",
-                  mode = { "n", "i" },
-                },
-              },
-            },
-          },
+  opts = {
+    dashboard = { enabled = false },
+    picker = {
+      matcher = {
+        frecency = true,
+      },
+      formatters = {
+        file = {
+          truncate = 80,
         },
-      })
-    end
-  end,
+      },
+      previewers = {
+        git = {
+          native = true,
+        },
+      },
+    },
+  },
 }
