1c95c9180b90ea892724cc2259077fbc2106633d
Ryan Cobb <rcobb@gitlab.com>
2025-04-08 09:29:43 -0700

    sync

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false }
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false }
 }
--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,25 +1,5 @@
 return {
   "folke/snacks.nvim",
-  keys = {
-    {
-      "<leader>fa",
-      function()
-        local file_name_no_ext, _ = vim.fn.expand("%:t:r"):gsub("_spec", "")
-        local relative_path, _ = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")
-
-        Snacks.picker.files({ args = { file_name_no_ext }, exclude = { relative_path } })
-      end,
-      desc = "alternate",
-    },
-    {
-      "<leader>fr",
-      function()
-        Snacks.picker.recent({ filter = { cwd = true } })
-      end,
-      desc = "Recent (cwd)",
-    },
-    { "<leader>fR", LazyVim.pick("oldfiles"), desc = "Recent" },
-  },
   opts = {
     dashboard = { enabled = false },
     picker = {
