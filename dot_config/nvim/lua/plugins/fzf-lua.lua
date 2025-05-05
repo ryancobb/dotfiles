1330c64998426dfdd85509c9025845cd2d5d1192
Ryan Cobb <rcobb@gitlab.com>
2025-05-05 14:28:52 -0700

    sync

---
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,7 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    { "<leader>sr", "<cmd>FzfLua resume<cr>", desc = "resume" },
+    { "<leader>st", LazyVim.pick("live_grep"), desc = "text" },
+  },
+}
