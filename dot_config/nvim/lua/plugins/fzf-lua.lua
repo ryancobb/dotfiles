56f5fa6e3872de89ed010b2ed755c6d20577010a
Ryan Cobb <rcobb@gitlab.com>
2025-05-15 12:49:56 -0700

    sync

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -1,5 +1,17 @@
 return {
   "ibhagwan/fzf-lua",
+  opts = {
+    files = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-files-history",
+      },
+    },
+    grep = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-grep-history",
+      },
+    },
+  },
   keys = {
     { "<leader>sr", "<cmd>FzfLua resume<cr>", desc = "resume" },
     { "<leader>st", LazyVim.pick("live_grep"), desc = "text" },
