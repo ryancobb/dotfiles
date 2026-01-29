2046d77af829ec8368178c8d849e72557a3b33b3
Ryan Cobb <rcobb@gitlab.com>
2026-01-29 11:42:05 -0700

    nvim: remap checkmate.nvim keybindings from <leader>T to <leader>t

--- dot_config/nvim/lua/plugins/checkmate.lua
+++ dot_config/nvim/lua/plugins/checkmate.lua
@@ -6,6 +6,21 @@ return {
 
     return {
       files = { "*.md" },
+      keys = {
+        ["<leader>tt"] = { rhs = "<cmd>Checkmate toggle<CR>", desc = "Toggle todo item", modes = { "n", "v" } },
+        ["<leader>tc"] = { rhs = "<cmd>Checkmate check<CR>", desc = "Set todo item as checked", modes = { "n", "v" } },
+        ["<leader>tu"] = { rhs = "<cmd>Checkmate uncheck<CR>", desc = "Set todo item as unchecked", modes = { "n", "v" } },
+        ["<leader>t="] = { rhs = "<cmd>Checkmate cycle_next<CR>", desc = "Cycle todo to next state", modes = { "n", "v" } },
+        ["<leader>t-"] = { rhs = "<cmd>Checkmate cycle_previous<CR>", desc = "Cycle todo to previous state", modes = { "n", "v" } },
+        ["<leader>tn"] = { rhs = "<cmd>Checkmate create<CR>", desc = "Create todo item", modes = { "n", "v" } },
+        ["<leader>tr"] = { rhs = "<cmd>Checkmate remove<CR>", desc = "Remove todo marker", modes = { "n", "v" } },
+        ["<leader>tR"] = { rhs = "<cmd>Checkmate remove_all_metadata<CR>", desc = "Remove all metadata", modes = { "n", "v" } },
+        ["<leader>ta"] = { rhs = "<cmd>Checkmate archive<CR>", desc = "Archive completed todos", modes = { "n" } },
+        ["<leader>tF"] = { rhs = "<cmd>Checkmate select_todo<CR>", desc = "Pick a todo", modes = { "n" } },
+        ["<leader>tv"] = { rhs = "<cmd>Checkmate metadata select_value<CR>", desc = "Update metadata value", modes = { "n" } },
+        ["<leader>t]"] = { rhs = "<cmd>Checkmate metadata jump_next<CR>", desc = "Next metadata tag", modes = { "n" } },
+        ["<leader>t["] = { rhs = "<cmd>Checkmate metadata jump_previous<CR>", desc = "Previous metadata tag", modes = { "n" } },
+      },
       todo_states = {
         unchecked = { marker = "[ ]" },
         checked = { marker = "[x]" },
@@ -19,6 +34,11 @@ return {
         CheckmateDeferredMainContent = { link = "CheckmateCheckedMainContent" },
         CheckmateDeferredAdditionalContent = { link = "CheckmateCheckedAdditionalContent" },
       },
+      metadata = {
+        priority = { key = "<leader>tp" },
+        started = { key = "<leader>ts" },
+        done = { key = "<leader>td" },
+      },
     }
   end,
   config = function(_, opts)
@@ -28,9 +48,5 @@ return {
       vim.api.nvim_set_hl(0, "CheckmateUncheckedMainContent", {})
       vim.api.nvim_set_hl(0, "CheckmateUncheckedAdditionalContent", {})
     end)
-
-    vim.keymap.set("n", "<leader>t>", function()
-      require("checkmate").toggle("deferred")
-    end, { desc = "Toggle deferred" })
   end,
 }
