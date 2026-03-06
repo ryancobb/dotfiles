588d2f2db4b0395d04e4c922bc8f2918a275a886
Ryan Cobb <rcobb@gitlab.com>
2026-03-06 10:42:39 -0700

    sync

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,29 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown" },
+  keys = {
+    {
+      "<leader>uM",
+      function()
+        local state = require("render-markdown.state")
+        local new_val = not state.config.anti_conceal.enabled
+        -- Update root config and all cached buffer configs
+        state.config.anti_conceal.enabled = new_val
+        -- Update concealcursor: '' allows anti-conceal to work, 'nvic' hides in all modes
+        local cc = new_val and "" or "nvic"
+        state.config.win_options.concealcursor.rendered = cc
+        for _, config in pairs(state.cache) do
+          config.anti_conceal.enabled = new_val
+          config.win_options.concealcursor.rendered = cc
+        end
+        -- Re-render to apply
+        vim.cmd("RenderMarkdown buf_disable")
+        vim.cmd("RenderMarkdown buf_enable")
+        vim.notify("Anti-conceal: " .. (new_val and "on" or "off"))
+      end,
+      desc = "Toggle render-markdown anti-conceal",
+    },
+  },
   opts = {
     preset = "lazy",
     -- anti_conceal = { enabled = false },
